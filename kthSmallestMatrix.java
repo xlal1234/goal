68238007e1256bd42ca6084a01433eee72e188bf
xlal1234 <526294596@qq.com>
2018-08-24 09:55:00 -0400

    use minHeap to compare

---
+++ kthSmallestMatrix.java
@@ -0,0 +1,42 @@
+public class Solution {
+  public int kthSmallest(int[][] matrix, int k) {
+    // Write your solution here
+    int rows = matrix.length;
+    int columns = matrix[0].length;
+    PriorityQueue<Cell> minHeap = new PriorityQueue<Cell>(k, new Comparator<Cell>() {
+        @Override
+        public int compare(Cell c1, Cell c2) {
+            if (c1.value == c2.value) {
+                return 0;
+            }
+            return c1.value < c2.value ? -1 : 1;
+        }
+    });
+    boolean[][] visited = new boolean[rows][columns];
+    minHeap.offer(new Cell(0, 0, matrix[0][0]));
+    visited[0][0] = true;
+    for(int i = 0; i < k - 1; i++) {
+        Cell cur = minHeap.poll();
+        if(cur.row + 1 < rows && !visited[cur.row + 1][cur.column]) {
+            minHeap.offer(new Cell(cur.row + 1, cur.column, matrix[cur.row + 1][cur.column]));
+            visited[cur.row+1][cur.column] = true;
+        }
+        if(cur.column + 1 < columns && !visited[cur.row][cur.column+1]) {
+            minHeap.offer(new Cell(cur.row, cur.column + 1, matrix[cur.row][cur.column + 1]));
+            visited[cur.row][cur.column + 1] = true;
+        }
+    }
+    return minHeap.peek().value;
+  }
+  static class Cell {
+      int row;
+      int column;
+      int value;
+
+      Cell(int row, int column, int value) {
+          this.row = row;
+          this.column = column;
+          this.value = value;
+      }
+  }
+}
